fa4e05be741a059602989a56554ed85ea96208e4
HeitorDelfino <hitoor.d@gmail.com>
2022-12-03 17:56:48 -0300

    SucessorAntecessor

---
+++ exe04.py
@@ -0,0 +1,9 @@
+#numero = int (input ('Digite um número:'))
+#n1 = numero -1
+#n2 = numero +1
+#print (' analisando o numero {}, seu antecessor é {}, e seu sucessor é {},' .format(numero, n1, n2))
+
+
+numero = int (input ('Digite um número:'))
+
+print (' analisando o numero {}, seu antecessor é {}, e seu sucessor é {},' .format(numero, numero-1, numero+1))
