0a928cf9852f6d3f1d84a70aecb58553906f0208
HeitorDelfino <hitoor.d@gmail.com>
2022-12-03 12:35:40 -0300

    VirgulaConcatenacao

---
+++ ex03.py
@@ -0,0 +1,5 @@
+#Utilizando virgulas para cocatenação
+n1 = int (input('Digite um valor:'))
+n2 = int (input('digite outro valor:'))
+s = (n1 + n2)
+print('a soma entre', n1, 'e', n2, 'é igual a', s)
