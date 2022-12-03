88caeb80d0ff327cee1506a153c3084d2e3b515d
HeitorDelfino <hitoor.d@gmail.com>
2022-12-03 12:42:42 -0300

    Alteracao

--- ex03.py
+++ ex03.py
@@ -1,5 +1,6 @@
-#Utilizando virgulas para cocatenação
+#Utilizando virgulas e .format{} para cocatenação
 n1 = int (input('Digite um valor:'))
 n2 = int (input('digite outro valor:'))
 s = (n1 + n2)
-print('a soma entre', n1, 'e', n2, 'é igual a', s)
+#print('a soma entre', n1, 'e', n2, 'é igual a', s)
+print('A soma ente {} e {} é igual a {}' .format(n1, n2, s))
